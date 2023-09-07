5f3738c1cd7290e123bbac2bc676373ff615c553
Althaf <Althaf@Althaf>
2023-09-07 21:56:28 +0530

    init commit

---
+++ demo-3/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into User values(1,20,'aaa');
+insert into User values(2,30,'bbb');
+insert into User values(3,40,'ccc');
+insert into User values(4,50,'ddd');
+insert into User values(5,60,'eee');
